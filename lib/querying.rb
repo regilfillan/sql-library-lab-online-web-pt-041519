80e02cd2e356bbafd16f1593853d82756bb17ed0
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-10 22:53:48 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
   JOIN characters
-  ON ;"
+  ON series.id = characters.series_id;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
