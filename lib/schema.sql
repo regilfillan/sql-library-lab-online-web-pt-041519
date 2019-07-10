a9aa078022605b427afddc7ff401a30757150dca
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-10 22:15:45 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,1 +1,15 @@
-CREATE TABLE
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,);
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY);
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER);
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT);
+CREATE TABLE sub_genres (
+  id INTEGER PRIMARY KEY,
+  name TEXT);
